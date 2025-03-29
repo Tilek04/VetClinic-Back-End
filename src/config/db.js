68369a388b45308f8f4bbe005e07432f9638f342
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2025-03-29 23:12:04 +0100

    Connect database and deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,10 @@
-import { Pool } from "pg";
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); 
+
+
+const { Pool } = pkg;
 
 export const pool = new Pool({
   user: process.env.PG_USER,
@@ -6,4 +12,7 @@ export const pool = new Pool({
   host: process.env.PG_HOST,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false }, // Если база на Render
 });
+
+console.log("PG_DATABASE_URL:", process.env.PG_DATABASE_URL);
--- src/models/createTables.js
+++ src/models/createTables.js
@@ -1,10 +1,35 @@
-import { Pool } from "pool";
+import pkg from "pg";
 import dotenv from "dotenv";
-import { connectionString } from "pg/lib/defaults";
 
 dotenv.config();
 
+const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.PG_DATABASE_URL
-})
+  connectionString: process.env.PG_DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
+});
+
+const createTables = async () => {
+  const query = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      role VARCHAR(20) CHECK (role IN ('patient', 'doctor', 'manager', 'admin')) NOT NULL DEFAULT 'patient',
+      verified BOOLEAN DEFAULT false
+    );
+  `;
+
+  try {
+    await pool.query(query);
+    console.log("Tables created successfully");
+  } catch (err) {
+    console.error("Error creating table:", err.message);
+  } finally {
+    pool.end();
+  }
+};
+
+createTables();
