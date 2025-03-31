287ac4ddfb6b23ff7a53a4ddb3da482aef4ae838
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2025-03-31 10:58:54 +0200

    Create router for login and done basic validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,12 @@
 import { pool } from "../config/db.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const userRegister = async (req, res) => {
   const { name, email, password, role } = req.body;
 
   if (!name || !email || !password) {
-    return res.status(400).json({ message: "Fill in the field!" });
+    return res.status(400).json({ message: "Fill in all fields!" });
   }
 
   try {
@@ -15,6 +16,32 @@ export const userRegister = async (req, res) => {
       [name, email, hashedPassword, role || "patient"]
     );
     res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Server error!" });
+  }
+};
+
+export const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Fill in all fields!" });
+  }
+
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE email = 1$`, [
+      email,
+    ]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials!" });
+    }
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ message: "Server error!" });
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers, userRegister } from "../controllers/userController.js"; // Проверьте правильность пути
+import { getUsers, userRegister } from "../controllers/userController.js"; 
 
 const router = express.Router();
 
 router.post("/register", userRegister);
 router.get("/", getUsers)
+router.post("/login", userLogin)
 
 export default router;
